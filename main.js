76c4c6ecc806917865b2788611104565a1a47b27
Henning Bertsch <henning.bertsch@q-perior.com>
2021-11-07 10:51:26 +0100

    Initial macOS release

--- main.js
+++ main.js
@@ -27,10 +27,9 @@ function createWindow() {
     //     slashes: true
     //   }));
       
-      
     
     //// uncomment below to open the DevTools.
-    win.webContents.openDevTools()
+    //win.webContents.openDevTools()
     
     // Event when the window is closed.
     win.on('closed', function () {
